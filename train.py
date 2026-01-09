bca0fba0a86a4c40ac9b8f1c061ad21a63bc6caa
2022bcs0123-lasyapriya <lasyabagadi@gmail.com>
2026-01-09 13:24:27 +0530

    Experiment 1: Linear Regression, test-split: 0.2, StandardScaler

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-model = Lasso(alpha=0.1)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
