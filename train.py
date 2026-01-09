16b6cea6f3f2cc6594b5173c4a0982dac6818466
2022bcs0123-lasyapriya <lasyabagadi@gmail.com>
2026-01-09 13:30:16 +0530

    Experiment 3: Random Forest (Estimators = 50, depth = 10)

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
 DATA_PATH = "dataset/winequality.csv"
@@ -29,7 +30,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-model = Ridge(alpha=1.0)
+model = RandomForestRegressor(n_estimators=50, max_depth=10, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
