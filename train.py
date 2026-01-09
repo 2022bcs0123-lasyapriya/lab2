0e3c1fbca9ed61f4593b526e053ad141f38ebb6e
2022bcs0123-lasyapriya <lasyabagadi@gmail.com>
2026-01-09 13:27:55 +0530

    Experiment 2: Ridge, alpha=0.1

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 
 DATA_PATH = "dataset/winequality.csv"
@@ -29,7 +29,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
